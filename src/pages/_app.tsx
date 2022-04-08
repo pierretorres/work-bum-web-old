28e6e146ea11c32c36cf8631f8f09f8547a63058
Pierre Torres <pierreptorres@gmail.com>
2022-04-08 15:35:10 -0300

    Delete src directory

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/global.css";
-
-import { ChallengesProvider } from "../contexts/ChallengesContext";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
